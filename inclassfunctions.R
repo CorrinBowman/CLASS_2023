ccb76eac1807754d17556faf43d250be26584607
Corrin Bowman <cobo9539@fiji-viz.colorado.edu>
2023-03-22 13:49:11 -0600

    3/22

---
+++ inclassfunctions.R
@@ -0,0 +1,11 @@
+
+
+#' @description 
+#' This function will multiply the input values of X and Y, put description of function 
+#' 
+#' @param x one number you'd like to multiply
+#' @param  y the other number you'd like to multiply
+fun <- function(x, y) {
+  ans <- x * y
+  return(ans)
+}
